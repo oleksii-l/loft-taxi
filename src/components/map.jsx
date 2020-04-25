f51b499b4f8982b7015042cb29c9204433b20dbc
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2020-04-25 23:09:06 +0300

    add MapBox

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/map.css";
+import mapboxgl from 'mapbox-gl';
 
-export default function Map() {
-  return <div className="map"></div>;
+mapboxgl.accessToken = 'pk.eyJ1Ijoib2xla3NpaS1sIiwiYSI6ImNrOWcxbHByZDBoMjEzbnFtanA5cXF4dDAifQ.Irs2VyQA206LVGQ0DyH8DA';
+
+export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 30.5290,
+      lat: 50.4487,
+      zoom: 13.5
+    };
+  }
+
+  render() {
+    return <div className='mapContainer' ref={el => this.mapContainer = el} />
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+    container: this.mapContainer,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [this.state.lng, this.state.lat],
+    zoom: this.state.zoom
+    });
+    }
+  
 }
