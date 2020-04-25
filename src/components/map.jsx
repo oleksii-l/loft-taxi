a2b26d3ea971051f47d16eadf5b8a38625e7cd30
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2020-04-25 23:27:47 +0300

    fix styles

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,30 +1,47 @@
 import React, { Component } from "react";
 import "../css/map.css";
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoib2xla3NpaS1sIiwiYSI6ImNrOWcxbHByZDBoMjEzbnFtanA5cXF4dDAifQ.Irs2VyQA206LVGQ0DyH8DA';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoib2xla3NpaS1sIiwiYSI6ImNrOWcxbHByZDBoMjEzbnFtanA5cXF4dDAifQ.Irs2VyQA206LVGQ0DyH8DA";
 
 export default class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: 30.5290,
+      lng: 30.529,
       lat: 50.4487,
-      zoom: 13.5
+      zoom: 13.5,
     };
   }
 
   render() {
-    return <div className='mapContainer' ref={el => this.mapContainer = el} />
+    return (
+      <div>
+        <div ref={(el) => (this.mapContainer = el)} className="mapContainer">
+          <div className="mapCoord">
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+            {this.state.zoom}
+          </div>
+        </div>
+      </div>
+    );
   }
 
   componentDidMount() {
     const map = new mapboxgl.Map({
-    container: this.mapContainer,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [this.state.lng, this.state.lat],
-    zoom: this.state.zoom
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
     });
-    }
-  
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }
 }
